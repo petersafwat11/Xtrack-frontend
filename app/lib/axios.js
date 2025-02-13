c209c8f4d9773df3475a766025f7ba02bb3ac73c
peter safwat <petersafwat640@gmail.com>
2025-02-13 09:26:20 +0200

    fix cors

--- app/lib/axios.js
+++ app/lib/axios.js
@@ -5,7 +5,20 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
   timeout: 10000, // 10 seconds
 });
+
+// Add response interceptor to handle CORS errors
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Handle unauthorized
+      console.log("Unauthorized access");
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
