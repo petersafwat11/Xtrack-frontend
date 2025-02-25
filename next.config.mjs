c8683129478b418f882636bdd806a948a547a9fc
peter safwat <petersafwat640@gmail.com>
2025-02-25 05:19:22 +0200

    switch server

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // BACKEND_SERVER: "https://xtrack-backenddd-production.up.railway.app",
+    BACKEND_SERVER: "https://xtrack-backenddd-production.up.railway.app",
     // BACKEND_SERVER: "https://web-production-6b66.up.railway.app",
-    BACKEND_SERVER: "http://localhost:5000",
+    // BACKEND_SERVER: "http://localhost:5000",
   },
   images: {
     remotePatterns: [
