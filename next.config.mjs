5eb812bcab098ac42d9e2b4d30f296890c10394b
peter safwat <petersafwat640@gmail.com>
2025-02-25 18:11:30 +0200

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,19 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/proxy/:path*',
-//         destination: 'http://178.128.210.208:8000/:path*', // Proxy to your backend
-//       },
-//     ];
-//   },
-//     env: {
-//     BACKEND_SERVER: "https://xtrack-backenddd-production.up.railway.app",
-//     // BACKEND_SERVER: "https://web-production-6b66.up.railway.app",
-//     // BACKEND_SERVER: "http://localhost:5000",
-//   },
-
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "xtrack-backend-production.up.railway.app",
-//       },
-//     ],
-//   },
-//   eslint: {
-//     // Warning: This allows production builds to successfully complete even if
-//     // your project has ESLint errors.
-//     ignoreDuringBuilds: true,
-//   },
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/proxy/:path*',
-  //       destination: 'http://178.128.210.208:8000/:path*', // Proxy to your backend
-  //     },
-  //   ];
-  // },
-  env: {
-    BACKEND_SERVER: "https://xtrack-backenddd-production.up.railway.app",
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/:path*',
+        destination: 'http://178.128.210.208:8000/:path*', // Proxy to your backend
+      },
+    ];
   },
+    env: {
+    BACKEND_SERVER: "https://xtrack-backenddd-production.up.railway.app",
+    // BACKEND_SERVER: "https://web-production-6b66.up.railway.app",
+    // BACKEND_SERVER: "http://localhost:5000",
+  },
+
   images: {
     remotePatterns: [
       {
@@ -52,8 +23,10 @@ const nextConfig = {
     ],
   },
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
