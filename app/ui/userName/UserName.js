9281647c6665e259e8241a507d063807c385e4fc
peter safwat <petersafwat640@gmail.com>
2025-03-04 02:19:58 +0200

    fixed errors in the application

--- app/ui/userName/UserName.js
+++ app/ui/userName/UserName.js
@@ -1,10 +1,11 @@
+"use client";
 import React from 'react'
 import Cookies from 'js-cookie';
 import styles from './username.module.css';
 const UserName = () => {
-  // const user = JSON.parse(Cookies.get('user'));
-  // const userName = user?.user_name
-  const userName = "Peter Safwat"
+  const user = Cookies.get('user')? JSON.parse(Cookies.get('user')) : null;
+  const userName = user?.user_name
+  // const userName = "Peter Safwat"
   return (
     <p className={styles.userName}>{userName}</p>
   )
